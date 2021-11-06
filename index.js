48d89332769eb59d863c2f201f12e5f688353327
Cosdaman <Cosdaguy@gmail.com>
2021-11-06 11:22:45 -0700

    file creation

--- index.js
+++ index.js
@@ -1,15 +1,78 @@
-// TODO: Include packages needed for this application
+//required packages
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//question array
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    // {
+    //     type: 'input',
+    //     message: 'What is your project description?',
+    //     name: 'description',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What are the installation instructions?',
+    //     name: 'install',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'What about usage information?',
+    //     name: 'usage',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'How about contribution guidelines?',
+    //     name: 'contribution',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Are there any test instructions?',
+    //     name: 'test',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Please choose a license from the following options: ',
+    //     name: 'license',
+    // },
+    // {
+    //     type: 'input',
+    //     message: "What's your Github username?",
+    //     name: 'github',
+    // },
+    // {
+    //     type: 'input',
+    //     message: "What's your email address?",
+    //     name: 'email',
+    // },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(
+        `${fileName}.md`,
+        `${data}\n`,
+        (err) => err ? console.error(err) : console.log('Commit logged!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log(response)
+            writeToFile(response.title, "")
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                prompt("error", error)
+            } else { prompt("something else went wrong") }
+        });
+}
 
 // Function call to initialize app
 init();
