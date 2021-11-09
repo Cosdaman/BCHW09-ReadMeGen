2455ab2ea8c0abb2485131681933a14ff99651ad
Cosdaman <Cosdaguy@gmail.com>
2021-11-09 12:27:55 -0800

    added licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -101,7 +101,7 @@ function renderLicenseSection(license) {
       along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
 
     case "GNU LGPL v3":
-      return `    This program is free software: you can redistribute it and/or modify
+      return `This program is free software: you can redistribute it and/or modify
       it under the terms of the GNU General Public License as published by
       the Free Software Foundation, either version 3 of the License, or
       (at your option) any later version.
@@ -115,10 +115,14 @@ function renderLicenseSection(license) {
       along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
 
     case "Mozilla Public License 2.0":
-      return;
+      return `This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.`;
 
     case "The MIT License":
-      return;
+      return `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
 
     default: return "";
   }
